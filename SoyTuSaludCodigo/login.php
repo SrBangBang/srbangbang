cc5ace716822cf7aa268cbe9b8fe47dbc745f71e
Oscar Labansat <oscar@digitalascent.mx>
2022-06-20 13:58:44 -0500

    Correct title for login page

--- SoyTuSaludCodigo/login.php
+++ SoyTuSaludCodigo/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Test</title>
+    <title>Login</title>
     <link rel="stylesheet" href="style.css"/>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <meta name="apple-mobile-web-app-capable" content="yes">
